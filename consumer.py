92d7c61faca2463c80f6f136e11b3b29f1addab9
João Hass <joaopedrohass@outlook.com>
2023-12-18 22:00:10 -0300

    feat  create first producer/consumer

---
+++ consumer.py
@@ -0,0 +1,34 @@
+import pika, sys, os
+
+def main():
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+
+    # Making sure that the queue exists
+    # idempotent command: we can run as many times as we want,
+    # only one 'hello' channel will be created
+    channel.queue_declare('hello')
+
+
+    def callback(ch, method, properties, body):
+        print(f'[x] receveid {body}')
+
+    channel.basic_consume(queue='hello',
+                        auto_ack=True,
+                        on_message_callback=callback)
+
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
+            
